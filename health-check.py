68779db2345be98696919ce996dcdc4268dabf5c
Keegan Lenz <Keegan.lenz@student.cart.org>
2022-01-30 23:00:49 -0800

    webdriver path to './chromedriver'

--- health-check.py
+++ health-check.py
@@ -3,62 +3,50 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import schedule
-import time
+
+driver = webdriver.Chrome('./chromedriver')
+
+driver.get("https://myclu.callutheran.edu/health-check/?_=1")
 
 
-def health_check():
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
+username = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="frmLogin_UserName"]')))
+password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="frmLogin_Password"]')))
+submit = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnLogin"]')))
 
-    driver.get("https://myclu.callutheran.edu/health-check/?_=1")
+username.send_keys('username')
+password.send_keys('password')
 
-
-    username = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="frmLogin_UserName"]')))
-    password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="frmLogin_Password"]')))
-    submit = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnLogin"]')))
-
-    username.send_keys('username')
-    password.send_keys('password')
-
-    driver.implicitly_wait(100)
-    submit.click()
+driver.implicitly_wait(100)
+submit.click()
 
 
-    campus_status = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[3]/div[2]/fieldset/div[2]/div[1]/button')))
-    campus_status.click()
+campus_status = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[3]/div[2]/fieldset/div[2]/div[1]/button')))
+campus_status.click()
 
-    driver.implicitly_wait(10)
+driver.implicitly_wait(10)
 
-    vacc_status1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[1]/div[2]/div[1]/button')))
-    vacc_status2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[2]/div[2]/div[2]/button')))
-    vacc_status3 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[3]/div[2]/div[2]/button')))
+vacc_status1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[1]/div[2]/div[1]/button')))
+vacc_status2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[2]/div[2]/div[2]/button')))
+vacc_status3 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[3]/div[2]/div[2]/button')))
 
-    vacc_status1.click()
-    vacc_status2.click()
-    vacc_status3.click()
+vacc_status1.click()
+vacc_status2.click()
+vacc_status3.click()
 
-    box1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="confirm_mask"]')))
-    box2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="confirm"]')))
+box1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="confirm_mask"]')))
+box2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="confirm"]')))
 
-    box1.click()
-    box2.click()
+box1.click()
+box2.click()
 
-    fname = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[5]/div[2]/div[3]/div[1]/label').text
-    lname = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[5]/div[2]/div[3]/div[2]/label').text
+fname = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[5]/div[2]/div[3]/div[1]/label').text
+lname = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[5]/div[2]/div[3]/div[2]/label').text
 
-    fbox = driver.find_element_by_xpath('//*[@id="signature_first_name"]')
-    lbox = driver.find_element_by_xpath('//*[@id="signature_last_name"]')
+fbox = driver.find_element_by_xpath('//*[@id="signature_first_name"]')
+lbox = driver.find_element_by_xpath('//*[@id="signature_last_name"]')
 
-    fbox.send_keys(fname)
-    lbox.send_keys(lname)
+fbox.send_keys(fname)
+lbox.send_keys(lname)
 
-    check = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[7]/button')
-    check.click()
-
-
-# Runs Automatic Health Check Program every day at 1:00 AM
-schedule.every().day.at('1:00').do(health_check)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+check = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[7]/button')
+check.click()
